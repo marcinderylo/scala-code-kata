1438a5b1d256a285fa3aafb409c5573e1bc224a9
Marcin Deryło <marcinderylo@gmail.com>
2012-09-24 21:35:22 +0200

    empty kata project structure

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "scala-kata"
+
+version := "1.0.0"
+
+scalaVersion := "2.9.2"
+
+scalacOptions ++= Seq("-deprecation")
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
---
+++ src/test/scala/KataTest.scala
@@ -0,0 +1,9 @@
+package com.blogspot.marcinderylo.kata
+
+import org.scalatest.FunSuite
+
+class KataTest extends FunSuite {
+        test("what would you like to test today?") {
+                fail("!!! just write a damn test !!!")
+        }
+}
