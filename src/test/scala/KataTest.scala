ec62de165701aae8fbd535c2fecc0c39decab7b9
Marcin Deryło <marcinderylo@gmail.com>
2012-09-29 15:47:50 +0200

    a bunch of immediately passing tests just to make sure we're on right way

--- src/test/scala/KataTest.scala
+++ src/test/scala/KataTest.scala
@@ -15,4 +15,13 @@ class KataTest extends FunSuite {
   test("prime factors of 4") {
     assert(PrimeFactors(4) === List(2, 2))
   }
+  test("prime factors of 5") {
+    assert(PrimeFactors(5) === List(5))
+  }
+  test("prime factors of 6") {
+    assert(PrimeFactors(6) === List(2, 3))
+  }
+  test("prime factors of 8") {
+    assert(PrimeFactors(8) === List(2, 2, 2))
+  }
 }
