b93d494aad00197424d44aeb95daec72338c6d15
Marcin Deryło <marcinderylo@gmail.com>
2012-09-29 15:42:59 +0200

    second failing test

--- src/test/scala/KataTest.scala
+++ src/test/scala/KataTest.scala
@@ -6,4 +6,7 @@ class KataTest extends FunSuite {
         test("prime factors of 1") {
                 assert(PrimeFactors(1) === Nil)
         }
+        test("prime factors of 2") {
+                assert(PrimeFactors(2) === List(2))
+        }
 }
