125c6afa04e465c4d8bf8e8d9a4b41d4c44d6fde
Marcin Deryło <marcinderylo@gmail.com>
2012-09-29 15:45:11 +0200

    reformatting code

--- src/main/scala/Kata.scala
+++ src/main/scala/Kata.scala
@@ -2,5 +2,9 @@ package com.blogspot.marcinderylo.kata
 
 object PrimeFactors {
 
-  def apply(n:Int) : List[Int] = if (n == 1) Nil else List(n)
+  def apply(n:Int) : List[Int] = 
+    if (n == 1) 
+      Nil 
+    else 
+      List(n)
 }
--- src/test/scala/KataTest.scala
+++ src/test/scala/KataTest.scala
@@ -3,16 +3,16 @@ package com.blogspot.marcinderylo.kata
 import org.scalatest.FunSuite
 
 class KataTest extends FunSuite {
-        test("prime factors of 1") {
-                assert(PrimeFactors(1) === Nil)
-        }
-        test("prime factors of 2") {
-                assert(PrimeFactors(2) === List(2))
-        }
-        test("prime factors of 3") {
-                assert(PrimeFactors(3) === List(3))
-        }
-        test("prime factors of 4") {
-                assert(PrimeFactors(4) === List(2, 2))
-        }
+  test("prime factors of 1") {
+    assert(PrimeFactors(1) === Nil)
+  }
+  test("prime factors of 2") {
+    assert(PrimeFactors(2) === List(2))
+  }
+  test("prime factors of 3") {
+    assert(PrimeFactors(3) === List(3))
+  }
+  test("prime factors of 4") {
+    assert(PrimeFactors(4) === List(2, 2))
+  }
 }
