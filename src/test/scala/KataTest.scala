becde31e4a2903dd55df0f800530c32bfc0fc5c8
Marcin Deryło <marcinderylo@gmail.com>
2012-09-29 15:44:22 +0200

    3rd test (immediately passing), 4th test - failing

--- src/test/scala/KataTest.scala
+++ src/test/scala/KataTest.scala
@@ -9,4 +9,10 @@ class KataTest extends FunSuite {
         test("prime factors of 2") {
                 assert(PrimeFactors(2) === List(2))
         }
+        test("prime factors of 3") {
+                assert(PrimeFactors(3) === List(3))
+        }
+        test("prime factors of 4") {
+                assert(PrimeFactors(4) === List(2, 2))
+        }
 }
