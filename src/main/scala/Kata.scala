0baa1f1df8cc87546dd8d7d172a4336edd178bc7
Marcin Deryło <marcinderylo@gmail.com>
2012-09-29 15:47:04 +0200

    making 4th test pass

--- src/main/scala/Kata.scala
+++ src/main/scala/Kata.scala
@@ -5,6 +5,9 @@ object PrimeFactors {
   def apply(n:Int) : List[Int] = 
     if (n == 1) 
       Nil 
-    else 
-      List(n)
+    else
+      if(n % 2 == 0) 
+        2 :: apply(n / 2) 
+      else
+        List(n)
 }
