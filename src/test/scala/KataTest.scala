1436bcfb8b74aaba2b30a01e3a5b306287362549
Marcin Deryło <marcinderylo@gmail.com>
2012-10-08 21:23:19 +0200

    initial code from the revised Scala exercises by Tony Morris' blogpost: http://blog.tmorris.net/revised-scala-exercises/

--- src/test/scala/KataTest.scala
+++ src/test/scala/KataTest.scala
@@ -3,7 +3,7 @@ package com.blogspot.marcinderylo.kata
 import org.scalatest.FunSuite
 
 class KataTest extends FunSuite {
-  test("what would you like to test today?") {
-    fail("!!! just write a damn test !!!")
+  test("first test, yet to be written'") {
+    fail("!!! just write it !!!")
   }
 }
