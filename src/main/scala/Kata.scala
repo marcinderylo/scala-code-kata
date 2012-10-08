5f74a757655ac4c60d6909e17505e76840661965
Marcin Deryło <marcinderylo@gmail.com>
2012-10-08 21:59:03 +0200

    implementing 3 first exercises (modified) - rest is the same as in scala-for-beginners branch *yawn* - except for maximum of course which would require extending the Natural class

--- src/main/scala/Kata.scala
+++ src/main/scala/Kata.scala
@@ -60,7 +60,10 @@ def ??? = throw new Exception("not implemented yet")
 // Performance: 0.5 mark
 // Elegance: 0.5 marks
 // Total: 3
-def add(x: Natural, y: Natural): Natural = ???
+def add(x: Natural, y: Natural): Natural = y match {
+  case Zero => x
+  case Succ(pred) => add(Succ(x), pred)
+}
  
 // Exercise 2
 // Relative Difficulty: 2
@@ -68,7 +71,7 @@ def add(x: Natural, y: Natural): Natural = ???
 // Performance: 1 mark
 // Elegance: 0.5 marks
 // Total: 4
-def sum(is: List[Int]): Int = ???
+def sum(is: List[Natural]): Natural = List.foldLeft(is, Zero, add)
  
 // Exercise 3
 // Relative Difficulty: 2
@@ -76,61 +79,8 @@ def sum(is: List[Int]): Int = ???
 // Performance: 1 mark
 // Elegance: 0.5 marks
 // Total: 4
-def length[A](as: List[A]): Int = ???
- 
-// Exercise 4
-// Relative Difficulty: 5
-// Correctness: 4.5 marks
-// Performance: 1.0 mark
-// Elegance: 1.5 marks
-// Total: 7
-def map[A, B](as: List[A], f: A => B): List[B] = ???
- 
-// Exercise 5
-// Relative Difficulty: 5
-// Correctness: 4.5 marks
-// Performance: 1.5 marks
-// Elegance: 1 mark
-// Total: 7
-def filter[A](as: List[A], f: A => Boolean): List[A] = ???
- 
-// Exercise 6
-// Relative Difficulty: 5
-// Correctness: 4.5 marks
-// Performance: 1.5 marks
-// Elegance: 1 mark
-// Total: 7
-def append[A](x: List[A], y: List[A]): List[A] = ???
- 
-// Exercise 7
-// Relative Difficulty: 5
-// Correctness: 4.5 marks
-// Performance: 1.5 marks
-// Elegance: 1 mark
-// Total: 7
-def flatten[A](as: List[List[A]]): List[A] = ???
- 
-// Exercise 8
-// Relative Difficulty: 7
-// Correctness: 5.0 marks
-// Performance: 1.5 marks
-// Elegance: 1.5 mark
-// Total: 8
-def flatMap[A, B](as: List[A], f: A => List[B]): List[B] = ???
- 
-// Exercise 9
-// Relative Difficulty: 8
-// Correctness: 3.5 marks
-// Performance: 3.0 marks
-// Elegance: 2.5 marks
-// Total: 9
-def maximum(is: List[Int]): Int = ???
- 
-// Exercise 10
-// Relative Difficulty: 10
-// Correctness: 5.0 marks
-// Performance: 2.5 marks
-// Elegance: 2.5 marks
-// Total: 10
-def reverse[A](as: List[A]): List[A] = ???
+def length[A](as: List[A]): Natural = as match {
+  case Empty => Zero
+  case Cons(_, tail) => Succ(length(tail))
+}
 }
--- src/test/scala/KataTest.scala
+++ src/test/scala/KataTest.scala
@@ -1,9 +1,30 @@
 package com.blogspot.marcinderylo.kata
 
 import org.scalatest.FunSuite
+import com.blogspot.marcinderylo.kata.Exercises._
 
 class KataTest extends FunSuite {
-  test("first test, yet to be written'") {
-    fail("!!! just write it !!!")
+  val One = Succ(Zero)
+  val Two = Succ(One)
+  val Three = Succ(Two)
+  val Four = Succ(Three)
+  val Five = Succ(Four)
+  val Six = Succ(Five)
+  val OneTwoThree = Cons(One, Cons(Two, Cons(Three, Empty))) 
+  test("add") {
+    assert(add(Zero, Zero) === Zero)
+    assert(add(Three, Zero) === Three)
+    assert(add(Zero, Two) === Two)
+    assert(add(Two, Four) === Six)
   }
+  test("sum") {
+    assert(sum(Empty) === Zero)
+    assert(sum(OneTwoThree) === Six)
+  }
+  test("length") {
+    assert(length(Empty) === Zero)
+    assert(length(Cons(One, Empty)) === One)
+    assert(length(OneTwoThree) === Three)
+  }
+
 }
